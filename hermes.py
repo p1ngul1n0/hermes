dbe00ee5489d4f6c5e7a97aadb1379693c4cc6bd
p1ngul1n0 <57546870+p1ngul1n0@users.noreply.github.com>
2022-05-19 20:23:29 -0300

    Add files via upload

--- hermes.py
+++ hermes.py
@@ -1,3 +1,4 @@
+from ast import For
 import time
 from bs4 import BeautifulSoup
 import json
@@ -20,12 +21,12 @@ parser.add_argument('-cpf', action = 'store', dest = 'cpf',
                            required = False,
                            help = 'CPF alvo.')
 parser.add_argument('--stealth',action='store_true',help='Busca CPF apenas em sites que não causam alguma ação como envio de e-mails ou SMS.')
+parser.add_argument('--list-sites', action = 'store_true', dest = 'list',
+                           required = False,
+                           help = 'Lista todos sites suportados.')
 arguments = parser.parse_args()
 
 proxy = "http://127.0.0.1:8080"
-headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 6.0; rv:40.0) Gecko/20100101 Firefox/40.0"
-}
 
 async def findUsername(cpf):
     start_time = time.time()
@@ -49,18 +50,25 @@ async def makeRequest(session,u,cpf):
     url = u["url"].format(cpf=cpf)
     jsonBody = None
     formData = None
+    headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 6.0; rv:40.0) Gecko/20100101 Firefox/40.0"
+    }
+    if 'formatted' in u and u['formatted']:
+        cpf = '{}.{}.{}-{}'.format(cpf[:3], cpf[3:6], cpf[6:9], cpf[9:])
     if 'headers' in u:
         headers.update(eval(u['headers']))
-    if 'json' in u:
+    if 'json' in u and '{cpf}' in u['json']:
         jsonBody = u['json'].format(cpf=cpf)
         jsonBody = json.loads(jsonBody)
-    if 'form' in u:
+    if 'form' in u and '{cpf}' in u['form']:
         formData = u['form'].format(cpf=cpf)
+    if 'string' in u:
+        formData = u['string']
     try:
         if arguments.stealth and u['silent'] == False:
             print (f'{Fore.CYAN}[+] - {u["app"]} não executado pois modo stealth está ativo - {url} \033[0m')
         else:
-            async with session.request(u["method"],url,json=jsonBody,data=formData, proxy=proxy,headers=headers, ssl=False) as response:
+            async with session.request(u["method"],url,json=jsonBody,proxy=proxy,data=formData,headers=headers, ssl=False) as response:
                 responseContent = await response.text()
                 
                 if 'content-type' in response.headers and "application/json" in response.headers["Content-Type"]:
@@ -69,19 +77,28 @@ async def makeRequest(session,u,cpf):
                     soup = BeautifulSoup(responseContent, 'html.parser')
                 
                 if eval(u["valid"]):
-                    print (f'{Fore.LIGHTGREEN_EX}[+] - {u["app"]} conta identificada - {url} [{response.status} {response.reason}]\033[0m')
+                    print (f'{Fore.LIGHTGREEN_EX}[+] - {u["app"]} conta identificada - {Fore.LIGHTCYAN_EX}{url}{Fore.RESET} [{response.status} {response.reason}]\033[0m')
+                    if 'returnData' in u:
+                        print (f'{Fore.CYAN}  |-----{eval(u["returnData"])}\033[0m')
                     return ({"app": u['app'], "url": url, "error": False, "found": True})
                 elif eval(u["error"]):
-                    print (f'{Fore.RED}[+] - {u["app"]} erro na consulta - {url} [{response.status} {response.reason}]\033[0m')
+                    print (f'{Fore.RED}[+] - {u["app"]} erro na consulta - {Fore.LIGHTCYAN_EX}{url}{Fore.RESET} [{response.status} {response.reason}]\033[0m')
                     return ({"app": u['app'], "url": url, "error": True, "found": False})
                 else:
-                    print (f'[-] - {u["app"]} conta não identificada - {url} [{response.status} {response.reason}]')
+                    print (f'[-] - {u["app"]} conta não identificada - {Fore.LIGHTCYAN_EX}{url}{Fore.RESET} [{response.status} {response.reason}]')
                     return ({"app": u['app'], "url": url, "error": False, "found": False})
     except Exception as e:
-        print (f'{Fore.RED}[X] - {u["app"]} erro na consulta ({repr(e)}) - {url}')
-        return ({"app": u['app'], "url": url, "error":True, "found": False})  
+        print (f'{Fore.RED}[X] - {u["app"]} erro na consulta ({repr(e)}) - {Fore.LIGHTCYAN_EX}{url}{Fore.RESET}\033[0m')
+        return ({"app": u['app'], "url": url, "error":True, "found": False, "error-message": {repr(e)} })  
          
+def list_sites():
+    i = 1
+    for u in searchData["sites"]:
+        print (f'{i}. {u["app"]}')
+        i += 1
 
 if arguments.cpf:
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-    asyncio.run(findUsername(arguments.cpf))   
+    asyncio.run(findUsername(arguments.cpf))
+elif arguments.list:
+    list_sites()
